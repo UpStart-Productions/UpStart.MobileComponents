92b522a407e9a87790aabe84bc16d3c5c81d93aa
Jeff Denton <chiefupstart@gmail.com>
2025-10-09 15:50:22 -0700

    Initial commit

---
+++ UpStart.MobileComponents/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.upstart.mobilecomponents',
+  appName: 'UpStart.MobileComponents',
+  webDir: 'www'
+};
+
+export default config;
